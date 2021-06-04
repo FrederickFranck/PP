721eaa4ccc407b5ae2a8ecd358603d1e4feddff4
Francky_ <frederick_spore@hotmail.com>
2021-06-04 20:16:21 +0200

    Finalised version : TODO: refresh clock

--- src/ppal.app.js
+++ src/ppal.app.js
@@ -1,65 +1,121 @@
-Modules.addCached("FontDylex7x13",function(){var a=atob("AAAAAAAAA/QAAcAAAHAAAAJAf4CQH+AkAAAMQJIP+CSBGAAAQAUIEYAwBiBCgAgAAG4EiCRA0gBgDIAAOAAAAfAwYgCAAIAjBgfAAACgAgB8AIAKAAABAAgB8AIAEAAAACAOAAAIAEACABAAgAAADAAAAYAwBgDAGAAAAfgQIJkECB+AAAIQIIP8ACABAAAQwQoIkEiBhAAAQgQIJEEiBuAAADACgCQCID/ACAAAeQJEEiCRBHAAAHwFEEiCRAHAAAQAIMEYCwBgAAANwJEEiCRA3AAAOAIkESCKA+AAAGYAAAAgzgAACACgCICCAAAKAFACgBQAoAAAggIgCgAgAABABAAjQSAGAAAA8AhAmQUoL0CKA4AAABwHAMgGQA4ADgAAf4JEEiCRA4gDgAADwCECBBAggQIQAAH+CBBAggQIQDwAAD/BIgkQSIJEECAAB/gkASAJAEAAAAeAQgQIIkESBOAAA/wCABAAgAQB/gAAQIP8ECAAABAAQQIIEH8AAB/gEADACQCECBAAA/wAIAEACABAAA/wMABgAwBgB/gAAf4MABgAMABg/wAADwCECBBAgQgHgAAH+CIBEAiAOAAAB4BCBAggQIQD2AAD/BEAiARgHIACAAAxAkQSIIkESBGAAAgAQAIAH+CABAAgAAAP4ACABAAgAQfwAAHAAcABgAwDgOAAAD4ADgGAMABgAOD4AAAwwEgBgAwAkBhgAAYACAAgAPAIAIAYAAAEGCFBEgkQUIMEAAH/yAJAEAAYADAAYADAAYAAQBIAn/wAAGAMAYADAAYAAAAIAEACABAAgAQAIAAQAEAAAADAKQFICkA+AAD/gIQEICEA8AAAPAIQEICEAkAAAPAIQEICEP+AAAPAKQFICkA0AAAQA/wkASAIAAAAPAISEJCEh/gAD/gIAEACAA+AAAQBPwAAABAAggSfwAA/4AQAYASAQgAAgAf8AAA/AQAIAD4CABAAfAAAPwEACABAAfAAAHgEICEBCAeAAAP+EICEBCAeAAAHgEICEBCA/4AAPwCACABAAQAAAEQFICkBKAiAAAIAfwCEBCABAAAPgAIAEACA/AAAMABgAMAYAwAAAPAAYAYAwAGABgPAAACEAkAMAJAIQAAD5ACQBIAkP8AACEBGAlAUgMQAAAgAQD3iAJAEAAf/AAEASAI94BAAgAAAIAIAEADAAgAQAQAAAFAHwFUCqBBARAAAACAOAAAAQQI/4kASAAAADgAAA4AAAEAAABAAAAQAAEACAH/AgAQAAAFACgH/AoAUAAAEAEAEABAAQAAAGMAYAwBjAAAAwAADEKRDIiiQRIEYAAAIAKAIgAAH4ECCBA/AkQSIIEAACDFChiRSIKEGCAADAAQAAAEAMAAADAAQAwAEAAABADAAQAwAAAAQAcAfAHABAAAAQAIAEACABAAAAQAIAEACABAAgAQAAAgAgAIAIAAACAB4AgAAAPAGADwAAAEQlIKkJKAiAAAIgCgAgAAAeAQgIQDwCkBSAaAAAIQkYKUJSAxAAAYACAQgAOEIAIAYAAAL8AAAeAQgf4EIBIAAATA+gkQSIAEAABBAfAIgEQCIB8BBAAAwAEgBQAeAUASAwAAAffAADCCYhKQjIIYAAEAAABAAAAH4ECCZBSgpQQIH4AAAQBUAqAPAAAAQAUAVAFAEQAAAQAIAEADwAAH4ECC9BUglQQIH4AAIAEACABAAgAQAIAAAAwAkASAGAAAAIgEQPoBEAiAACIBMAqAJAAAEQCoBUAUAAAEAEAAAAAEH8AIACABAfAAQAAGAHgD/hAA/4QAAAA4AcAOAAAAFADAACQD4AEAAAOAIgEQBwAAAEQBQBUAUAEAAA8YAwBkDGGHgAgAAeMAYAwBpjFQBIAAIgFTB2AMgYww8AEAAADACQWIAEAEAAADgOBJAUgBwAHAAABwHAUgSQA4ADgAAA4TgSQJICcABwAAAcJwJICkCOAA4AAAOE4AkASAnAAcAAAHDcCSBJAbgAOAAADgGANAIgH+CRBAgAAHgEIECSBxAgQgAAH8SSFJAkgQQAAH8CSFJEkgQQAAH8KSJJCkgQQAAH8KSBJCkgQQAAEET+FBAAAQQv4kEAAFBE/hQQAAUED+FBAAACAP4EkCSBBARAHAAAH8KAIwCGCAwP4AAA4AiEghQQEQBwAAAcARBQRIICIA4AAAOBIhIIkEJEAcAAAHAkQkEKCIiAOAAADgSICCBBCRAHAAACIAoAIAKAIgAAD0CECNBYgQgXgAAD8ABEAhAQAIH4AAB+AAhARAIAED8AAA/BARAIgEICB+AAAfggIAEACEBA/AAAMABAAQEHEEAEAMAAAH+AkASAJADAAAABD/CQhIQkINEAcAAADAKQlIKkA+AAADAKQVISkA+AAADAqQlIKkA+AAADAqQlIKkI+AAADAqQFIKkA+AAADBKRVISkA+AAADAKQFIB8BSApANAAADwCEhDghAJAAADwSkFSApANAAADwKkJSApANAAADwKkJSCpANAAADwKkBSCpANAAAkAL8AACgCfgAAUAT8EAAACQAPwgAAAAcERCogkQvwAAF+EgBQBIAD4AAA8EhBQgIQDwAAA8AhBQhIQDwAAA8ChCQgoQDwAAA8ChCQgoQjwAAA8ChAQgoQDwAAAQAIAVACABAAAA9AjAWgMQLwAAB8EBBAgAQH4AAB8CBCAgAQH4AAB8CBCAggQH4AAB8CBAAggQH4AAB8ABJAlASH+AAH/ghAQgIQDwAAB8CBIAkgSH+AAA"),
-b=atob("AwIEBgYIBwIEBAYGAwYCBgYGBgYHBgYGBgYCAwUGBQYIBwcHBwcGBwcEBgcGBwcHBgcHBwgHBwgHCAcEBgQGCAMGBgYGBgYGBgMFBgMIBgYGBgYGBgYGCAYGBgYCBggABwADBgQGBgYGBwcECAAHAAADAwUFBgYIBQgGBAgABggAAgYGCAgCBgQIBQYFAAgIBQYFBQMIBwQDBAUGBwcIBgcHBwcHBwgHBgYGBgQEBAQIBwcHBwcHBgcHBwcHCAYIBgYGBgYGCAYGBgYGAwMEBAYGBgYGBgYGBgYGBgYGBgY=");exports.add=function(c){c.prototype.setFontDylex7x13=function(){this.setFontCustom(a,32,b,13)}}});
+Modules.addCached("FontDylex7x13", function () {
+  var a = atob("AAAAAAAAA/QAAcAAAHAAAAJAf4CQH+AkAAAMQJIP+CSBGAAAQAUIEYAwBiBCgAgAAG4EiCRA0gBgDIAAOAAAAfAwYgCAAIAjBgfAAACgAgB8AIAKAAABAAgB8AIAEAAAACAOAAAIAEACABAAgAAADAAAAYAwBgDAGAAAAfgQIJkECB+AAAIQIIP8ACABAAAQwQoIkEiBhAAAQgQIJEEiBuAAADACgCQCID/ACAAAeQJEEiCRBHAAAHwFEEiCRAHAAAQAIMEYCwBgAAANwJEEiCRA3AAAOAIkESCKA+AAAGYAAAAgzgAACACgCICCAAAKAFACgBQAoAAAggIgCgAgAABABAAjQSAGAAAA8AhAmQUoL0CKA4AAABwHAMgGQA4ADgAAf4JEEiCRA4gDgAADwCECBBAggQIQAAH+CBBAggQIQDwAAD/BIgkQSIJEECAAB/gkASAJAEAAAAeAQgQIIkESBOAAA/wCABAAgAQB/gAAQIP8ECAAABAAQQIIEH8AAB/gEADACQCECBAAA/wAIAEACABAAA/wMABgAwBgB/gAAf4MABgAMABg/wAADwCECBBAgQgHgAAH+CIBEAiAOAAAB4BCBAggQIQD2AAD/BEAiARgHIACAAAxAkQSIIkESBGAAAgAQAIAH+CABAAgAAAP4ACABAAgAQfwAAHAAcABgAwDgOAAAD4ADgGAMABgAOD4AAAwwEgBgAwAkBhgAAYACAAgAPAIAIAYAAAEGCFBEgkQUIMEAAH/yAJAEAAYADAAYADAAYAAQBIAn/wAAGAMAYADAAYAAAAIAEACABAAgAQAIAAQAEAAAADAKQFICkA+AAD/gIQEICEA8AAAPAIQEICEAkAAAPAIQEICEP+AAAPAKQFICkA0AAAQA/wkASAIAAAAPAISEJCEh/gAD/gIAEACAA+AAAQBPwAAABAAggSfwAA/4AQAYASAQgAAgAf8AAA/AQAIAD4CABAAfAAAPwEACABAAfAAAHgEICEBCAeAAAP+EICEBCAeAAAHgEICEBCA/4AAPwCACABAAQAAAEQFICkBKAiAAAIAfwCEBCABAAAPgAIAEACA/AAAMABgAMAYAwAAAPAAYAYAwAGABgPAAACEAkAMAJAIQAAD5ACQBIAkP8AACEBGAlAUgMQAAAgAQD3iAJAEAAf/AAEASAI94BAAgAAAIAIAEADAAgAQAQAAAFAHwFUCqBBARAAAACAOAAAAQQI/4kASAAAADgAAA4AAAEAAABAAAAQAAEACAH/AgAQAAAFACgH/AoAUAAAEAEAEABAAQAAAGMAYAwBjAAAAwAADEKRDIiiQRIEYAAAIAKAIgAAH4ECCBA/AkQSIIEAACDFChiRSIKEGCAADAAQAAAEAMAAADAAQAwAEAAABADAAQAwAAAAQAcAfAHABAAAAQAIAEACABAAAAQAIAEACABAAgAQAAAgAgAIAIAAACAB4AgAAAPAGADwAAAEQlIKkJKAiAAAIgCgAgAAAeAQgIQDwCkBSAaAAAIQkYKUJSAxAAAYACAQgAOEIAIAYAAAL8AAAeAQgf4EIBIAAATA+gkQSIAEAABBAfAIgEQCIB8BBAAAwAEgBQAeAUASAwAAAffAADCCYhKQjIIYAAEAAABAAAAH4ECCZBSgpQQIH4AAAQBUAqAPAAAAQAUAVAFAEQAAAQAIAEADwAAH4ECC9BUglQQIH4AAIAEACABAAgAQAIAAAAwAkASAGAAAAIgEQPoBEAiAACIBMAqAJAAAEQCoBUAUAAAEAEAAAAAEH8AIACABAfAAQAAGAHgD/hAA/4QAAAA4AcAOAAAAFADAACQD4AEAAAOAIgEQBwAAAEQBQBUAUAEAAA8YAwBkDGGHgAgAAeMAYAwBpjFQBIAAIgFTB2AMgYww8AEAAADACQWIAEAEAAADgOBJAUgBwAHAAABwHAUgSQA4ADgAAA4TgSQJICcABwAAAcJwJICkCOAA4AAAOE4AkASAnAAcAAAHDcCSBJAbgAOAAADgGANAIgH+CRBAgAAHgEIECSBxAgQgAAH8SSFJAkgQQAAH8CSFJEkgQQAAH8KSJJCkgQQAAH8KSBJCkgQQAAEET+FBAAAQQv4kEAAFBE/hQQAAUED+FBAAACAP4EkCSBBARAHAAAH8KAIwCGCAwP4AAA4AiEghQQEQBwAAAcARBQRIICIA4AAAOBIhIIkEJEAcAAAHAkQkEKCIiAOAAADgSICCBBCRAHAAACIAoAIAKAIgAAD0CECNBYgQgXgAAD8ABEAhAQAIH4AAB+AAhARAIAED8AAA/BARAIgEICB+AAAfggIAEACEBA/AAAMABAAQEHEEAEAMAAAH+AkASAJADAAAABD/CQhIQkINEAcAAADAKQlIKkA+AAADAKQVISkA+AAADAqQlIKkA+AAADAqQlIKkI+AAADAqQFIKkA+AAADBKRVISkA+AAADAKQFIB8BSApANAAADwCEhDghAJAAADwSkFSApANAAADwKkJSApANAAADwKkJSCpANAAADwKkBSCpANAAAkAL8AACgCfgAAUAT8EAAACQAPwgAAAAcERCogkQvwAAF+EgBQBIAD4AAA8EhBQgIQDwAAA8AhBQhIQDwAAA8ChCQgoQDwAAA8ChCQgoQjwAAA8ChAQgoQDwAAAQAIAVACABAAAA9AjAWgMQLwAAB8EBBAgAQH4AAB8CBCAgAQH4AAB8CBCAggQH4AAB8CBAAggQH4AAB8ABJAlASH+AAH/ghAQgIQDwAAB8CBIAkgSH+AAA"),
+  b = atob("AwIEBgYIBwIEBAYGAwYCBgYGBgYHBgYGBgYCAwUGBQYIBwcHBwcGBwcEBgcGBwcHBgcHBwgHBwgHCAcEBgQGCAMGBgYGBgYGBgMFBgMIBgYGBgYGBgYGCAYGBgYCBggABwADBgQGBgYGBwcECAAHAAADAwUFBgYIBQgGBAgABggAAgYGCAgCBgQIBQYFAAgIBQYFBQMIBwQDBAUGBwcIBgcHBwcHBwgHBgYGBgQEBAQIBwcHBwcHBgcHBwcHCAYIBgYGBgYGCAYGBgYGAwMEBAYGBgYGBgYGBgYGBgYGBgY="); exports.add = function (c) { c.prototype.setFontDylex7x13 = function () { this.setFontCustom(a, 32, b, 13) } }
+});
 require("FontDylex7x13").add(Graphics);
 
-var NextFerry = 0;
+
+var UI_Next = "";
+var UI_Mode = "Ferry:";
 const X = 0, Y = 80;
+var GPS = 0;
+var UI_ETA = "";
+
+var __lat;
+var __long;
+var __speed;
+
+
 
 function draw() {
+  g.clear();
   g.reset();
   g.setFont("Dylex7x13", 2);
-  //g.setFont("Vector", 20); // vector font, 80px  
-  // draw the current counter value
+
   var d = new Date();
   var h = d.getHours(), m = d.getMinutes();
   var time = (" " + h).substr(-2) + ":" + ("0" + m).substr(-2);
 
+  g.drawString("GPS:", X, Y - 40);
+  g.drawString(GPS, X + 60, Y - 40, true);
+
   g.drawString("Time :", X, Y);
-  g.drawString(time, X +80 , Y, true);
-  g.drawString("Next Ferry:", X, Y + 80);
-  g.drawString(NextFerry, X + 130, Y + 80, true);
+  g.drawString(time, X + 80, Y, true);
+
+  g.drawString("Next " + UI_Mode, X, Y + 80);
+  g.drawString(UI_Next, X + 130, Y + 80, true);
+
+  g.drawString("ETA:", X, Y + 120);
+  g.drawString(UI_ETA, X + 60, Y + 120, true);
 
   Bangle.loadWidgets();
   Bangle.drawWidgets();
 
 }
 
-function updateTime(){
-d = new Date();
-epoch = Math.round(d.getTime());
-NRF.updateServices({
-      0x66FF: {
-        0x0003: {
-          value: epoch.toString(),
-        }
-      }
-    });
-}
 
-function updateFerry(buffer) {
-  ferrytime ="";
-  for (let x of buffer){
-    let c = String.fromCharCode(x);
-    ferrytime.concat(c);
+
+function updateNext(buffer) {
+  UI_Next = "";
+  Terminal.println(buffer);
+  for (var i = 0; i < buffer.length; i++) {
+    UI_Next += String.fromCharCode(buffer[i]);
   }
-  NextFerry = ferrytime;
+  //Terminal.println(UI_Next);
+
+  draw();
+}
+
+function updateETA(buffer) {
+  UI_ETA = "";
+  Terminal.println(buffer);
+  for (var i = 0; i < buffer.length; i++) {
+    UI_ETA += String.fromCharCode(buffer[i]);
+  }
+  //Terminal.println(UI_ETA);
+  draw();
+}
+
+//updates services with last know values to trigger an update
+function forceUpdate(){
+  NRF.updateServices({
+    0x66FF: {
+      0x0001: {
+        value: __lat,
+      }
+    }
+  });
+
+  NRF.updateServices({
+    0x66FF: {
+      0x0002: {
+        value: __long,
+      }
+    }
+  });
+
+  NRF.updateServices({
+    0x66FF: {
+      0x0004: {
+        value: __speed,
+      }
+    }
+  });
 
 }
+
+
 function onGPS(fix) {
   hasfix = fix.fix;
   if (hasfix) {
     round = 5;
-    let _lat = fix.lat.toFixed(round);
-    let _long = fix.lon.toFixed(round);
-    let _speed = fix.speed.toFixed(round);
-    //Terminal.println("lat: " + _lat);
-    //Terminal.println("long: " + _long);
-    //Terminal.println("speed: " + _speed);
+    let lat = fix.lat.toFixed(round);
+    let long = fix.lon.toFixed(round);
+    let speed = fix.speed.toFixed(round);
+    if (GPS == 0) {
+      GPS = 1;
+      draw();
+    }
+
+    __lat = lat;
+    __long = long;
+    __speed = speed;
+
 
     NRF.updateServices({
       0x66FF: {
         0x0001: {
-          value: _lat,
+          value: lat,
         }
       }
     });
@@ -67,15 +123,7 @@ function onGPS(fix) {
     NRF.updateServices({
       0x66FF: {
         0x0002: {
-          value: _long,
-        }
-      }
-    });
-
-    NRF.updateServices({
-      0x66FF: {
-        0x0003: {
-          value: fix.time.getTime().toString(),
+          value: long,
         }
       }
     });
@@ -83,7 +131,7 @@ function onGPS(fix) {
     NRF.updateServices({
       0x66FF: {
         0x0004: {
-          value: _speed,
+          value: speed,
         }
       }
     });
@@ -99,21 +147,18 @@ function initServices() {
       0x0001: {
         value: (51.174070).toString(), // optional
         readable: true,   // optional, default is false
-        writable: false,   // optional, default is false
         notify: true,   // optional, default is false
         description: "latitude"  // optional, default is null,
       },
       0x0002: {
         value: (4.380827).toString(), // optional
         readable: true,   // optional, default is false
-        writable: false,   // optional, default is false
         notify: true,   // optional, default is false
         description: "longitude"  // optional, default is null,
       },
       0x0003: {
         value: "1616162138000", // optional
         readable: true,   // optional, default is false
-        writable: false,   // optional, default is false
         notify: true,   // optional, default is false
         description: "time"  // optional, default is null,
       },
@@ -121,13 +166,8 @@ function initServices() {
         value: "78178178", // optional
         broadcast: true, // optional, default is false
         readable: true,   // optional, default is false
-        writable: true,   // optional, default is false
         notify: true,   // optional, default is false
         description: "Speed",  // optional, default is null,
-
-        onWrite: function (evt) { // optional
-          E.showMessage("0004 ", evt.data); // an ArrayBuffer
-        }
       },
       0x0005: {
         value: "Ferry", // optional
@@ -135,7 +175,7 @@ function initServices() {
         readable: true,   // optional, default is false
         writable: true,   // optional, default is false
         notify: true,   // optional, default is false
-        description: "Description of next public transport",  // optional, default is null,
+        description: "Description of UI_Next public transport",  // optional, default is null,
 
         onWrite: function (evt) { // optional
           console.log("0005 ", evt.data); // an ArrayBuffer
@@ -147,16 +187,10 @@ function initServices() {
         readable: true,   // optional, default is false
         writable: true,   // optional, default is false
         notify: true,   // optional, default is false
-        description: "Time when next public transport leaves",  // optional, default is null,
+        description: "Time when UI_Next public transport leaves",  // optional, default is null,
 
         onWrite: function (evt) { // optional
-          NextFerry = "";
-          Terminal.println(evt);
-          for(var i =0; i < evt.data.length; i++){
-            NextFerry += String.fromCharCode(evt.data[i]);
-          }          
-          draw();
-          //draw();
+          updateNext(evt.data);
         }
       },
       0x0007: {
@@ -168,7 +202,7 @@ function initServices() {
         description: "Estimated time of arrival",  // optional, default is null,
 
         onWrite: function (evt) { // optional
-          console.log("Got ", evt.data); // an ArrayBuffer
+          updateETA(evt.data);
         }
       }
       // more characteristics allowed
@@ -183,13 +217,20 @@ function init() {
   initServices();
   Bangle.on('GPS', onGPS);
   draw();
-  var timestampInterval = setInterval(updateTime, 1000);
-  
-  
+  //var timestampInterval = setInterval(updateTime, 1000);
+
+
   setWatch(() => {
- draw();
-}, BTN2, {repeat:true});
-   
+    draw();
+  }, BTN1, { repeat: true });
+
+  // Show launcher when middle button pressed
+  setWatch(Bangle.showLauncher, BTN2, { repeat: false, edge: "falling" });
+
+  setWatch(() => {
+    forceUpdate();
+  }, BTN3, { repeat: true });
 }
+
 
 init();
